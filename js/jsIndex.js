b7a66ad93c00ba7ad5a9a86ae316e6059ef286b8
ManuMontaraz <montarazmanu@gmail.com>
2024-10-13 02:37:30 +0200

    update functionality and style

---
+++ js/jsIndex.js
@@ -0,0 +1,56 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    create_events()
+    open_section()
+})
+
+function get_searches(){
+    const Searches = document.location.search.slice(1).split("&")
+    const SearchesArray = []
+    for(let indexSearches = 0 ; indexSearches < Searches.length ; indexSearches++){
+        const Search = Searches[indexSearches].split("=")
+        if(Search[0] != "")SearchesArray.push({"name":Search[0],"value":atob(Search[1].replaceAll("*","="))})
+    }
+    if(SearchesArray.length == 0)return false
+    return SearchesArray
+}
+
+function open_section(){
+    const Search = get_searches()
+    console.log("Search",Search)
+
+    let openThisID = ""
+
+    if(Search){
+        const NameValue = Search.find((search)=>search.name == "name").value
+        console.log("NameValue",NameValue)
+    
+        const SecretValue = Search.find((search)=>search.name == "secret").value
+        console.log("SecretValue",SecretValue)
+    
+        openThisID = "content"
+        
+    }else{
+        openThisID = "login"
+    }
+
+    const OpenThis = document.querySelector(`#${openThisID}`)
+    OpenThis.classList.add("opening")
+    OpenThis.addEventListener("transitionend",()=>{
+        OpenThis.classList.add("active")
+        OpenThis.classList.remove("opening")
+    })
+}
+
+function create_events(){
+    const FormLogin = document.querySelector("#login_form")
+    FormLogin.querySelector("input[type=submit]").addEventListener("click",event=>{
+        //event.preventDefault()
+        const NameValue = FormLogin.name.value
+        const SecretValue = FormLogin.secret.value
+        const PasswordValue = FormLogin.password.value
+        if(NameValue == "" || SecretValue == "")event.preventDefault()
+        FormLogin.name.value = btoa(FormLogin.name.value).replaceAll("=","*")
+        FormLogin.secret.value = btoa(FormLogin.secret.value).replaceAll("=","*")
+        FormLogin.password.value = btoa(FormLogin.password.value).replaceAll("=","*")
+    })
+}
