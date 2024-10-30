04ef5e5921742d95c12c67a1a8622a755dd73b41
ManuMontaraz <montarazmanu@gmail.com>
2024-10-30 13:00:16 +0100

    update js

--- js/jsIndex.js
+++ js/jsIndex.js
@@ -1,7 +1,18 @@
 document.addEventListener("DOMContentLoaded",()=>{
+    init_veggie()
+})
+
+function init_veggie(){
+    document.veggie = {
+        server:{
+            secret:"",
+            password:""
+        },
+        users:[]
+    }
     create_events()
     open_section()
-})
+}
 
 function get_searches(){
     const Searches = document.location.search.slice(1).split("&")
@@ -18,19 +29,30 @@ function open_section(){
     const Search = get_searches()
     console.log("Search",Search)
 
+    const NameValue = Search?Search.find((search)=>search.name == "name")?Search.find((search)=>search.name == "name").value:"":""
+    document.veggie.users.push(NameValue)
+    console.log("NameValue",NameValue)
+
+    const SecretValue = Search?Search.find((search)=>search.name == "secret")?Search.find((search)=>search.name == "secret").value:"":""
+    document.veggie.server.secret = SecretValue
+    console.log("SecretValue",SecretValue)
+
+    const PasswordValue = Search?Search.find((search)=>search.name == "password")?Search.find((search)=>search.name == "password").value:"":""
+    document.veggie.server.password = PasswordValue
+    console.log("PasswordValue",PasswordValue)
+
     let openThisID = ""
 
-    if(Search){
-        const NameValue = Search.find((search)=>search.name == "name").value
-        console.log("NameValue",NameValue)
-    
-        const SecretValue = Search.find((search)=>search.name == "secret").value
-        console.log("SecretValue",SecretValue)
+    if(NameValue && SecretValue){
     
         openThisID = "content"
+
+        open_content()
         
     }else{
         openThisID = "login"
+        document.querySelector("#login_form #secret").value = SecretValue
+        document.querySelector("#login_form #password").value = PasswordValue
         document.querySelector("#logo").classList.add("open")
     }
 
@@ -45,13 +67,41 @@ function open_section(){
 function create_events(){
     const FormLogin = document.querySelector("#login_form")
     FormLogin.querySelector("input[type=submit]").addEventListener("click",event=>{
-        //event.preventDefault()
+        event.preventDefault()
         const NameValue = FormLogin.name.value
         const SecretValue = FormLogin.secret.value
         const PasswordValue = FormLogin.password.value
-        if(NameValue == "" || SecretValue == "")event.preventDefault()
+
+        if(NameValue == "" || SecretValue == "")return
+        
+        let searchString = ""
+        if(NameValue)searchString += `${searchString==""?"?":"&"}name=${btoa(NameValue).replaceAll("=","*")}`
+        if(SecretValue)searchString += `${searchString==""?"?":"&"}secret=${btoa(SecretValue).replaceAll("=","*")}`
+        if(PasswordValue)searchString += `${searchString==""?"?":"&"}password=${btoa(PasswordValue).replaceAll("=","*")}`
+
+        location.search = searchString
+
+        /*
         FormLogin.name.value = btoa(FormLogin.name.value).replaceAll("=","*")
         FormLogin.secret.value = btoa(FormLogin.secret.value).replaceAll("=","*")
         FormLogin.password.value = btoa(FormLogin.password.value).replaceAll("=","*")
+        */
+
+        console.log(searchString)
     })
+}
+
+function open_content(){
+    const ContainerNameServer = document.querySelector("#name_server")
+    const ContainerPasswordServer = document.querySelector("#password_server")
+    const ContainerUsersOnline = document.querySelector("#users_online")
+
+    ContainerNameServer.textContent = document.veggie.server.secret
+    if(document.veggie.server.password){
+        ContainerPasswordServer.textContent = "true"
+        ContainerPasswordServer.classList.add("ok")
+    }else{
+        ContainerPasswordServer.textContent = "false"
+        ContainerPasswordServer.classList.add("ko")
+    }
 }
